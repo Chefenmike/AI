b1ba832e267d9f7d4839c71258982bbdf3f6d445
unknown <johfr@ED-2480-10.net.chalmers.se>
2014-04-02 13:01:52 +0200

    Implemented the World Class

--- src/group3/definitions/World.java
+++ src/group3/definitions/World.java
@@ -1,5 +1,32 @@
 package group3.definitions;
 
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 public class World {
+
+	private ArrayList worldRepresentationList;
+
+	public World(JSONArray world, JSONObject objects) {
+		worldRepresentationList = new ArrayList<ObjectInWorld>();
+		for (int i = 0; i < world.size(); i++) {
+			ArrayList columnList = new ArrayList<ObjectInWorld>();
+			JSONArray column = (JSONArray) world.get(i);
+
+			for (int j = 0; j < column.size(); j++) {
+				JSONObject obj = (JSONObject) objects.get(column.get(j));
+				ObjectInWorld oiw = new ObjectInWorld(
+						Shape.getShapeValueFromString((String) obj.get("form")),
+						Colour.getColourValueFromString((String) obj
+								.get("colour")), Size
+								.getSizeValueFromString((String) obj
+										.get("size")));
+				columnList.add(oiw);
+			}
+			worldRepresentationList.add(columnList);
+		}
+	}
 
 }
