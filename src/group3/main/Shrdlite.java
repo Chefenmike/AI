7b7df4f1d7db1f7f1de6afad2665393fdef905b4
Chefenmike <Mikael666jo@hotmail.com>
2014-04-03 23:36:16 +0200

    Added possibility to test in console. For now; just a small world.

--- src/group3/main/Shrdlite.java
+++ src/group3/main/Shrdlite.java
@@ -28,16 +28,49 @@ import org.json.simple.JSONArray;
 
 public class Shrdlite {
 
-	public static void main(String[] args) throws PrologException, ParseException, IOException {
-        JSONObject jsinput   = (JSONObject) JSONValue.parse(readFromStdin());
+	private static boolean consoleTest = true;
+	private static boolean smallWorld = true;
+	public static void main(String[] args) throws PrologException, ParseException, IOException {	
+		JSONObject jsinput;
+		if(consoleTest) {
+		String takeCmd = "";
+			try{
+			    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			    //Use this form for now: ["take","the","white","ball"]
+			    if ((takeCmd = in.readLine()) != null && takeCmd.length() != 0){
+			      //System.out.println(takeCmd);
+			    }
+			  }
+			catch(IOException io){
+				io.printStackTrace();
+			}
+			
+			if(smallWorld) { 
+				jsinput   = (JSONObject) JSONValue.parse("{\"world\":[[\"e\"],[\"g\",\"l\"],[],[\"k\",\"m\",\"f\"],[]],\"objects\":{\"a\":{\"form\":\"brick\",\"size\":\"large\",\"color\":\"green\"},\"b\":{\"form\":\"brick\",\"size\":\"small\",\"color\":\"white\"},\"c\":{\"form\":\"plank\",\"size\":\"large\",\"color\":\"red\"},\"d\":{\"form\":\"plank\",\"size\":\"small\",\"color\":\"green\"},\"e\":{\"form\":\"ball\",\"size\":\"large\",\"color\":\"white\"},\"f\":{\"form\":\"ball\",\"size\":\"small\",\"color\":\"black\"},\"g\":{\"form\":\"table\",\"size\":\"large\",\"color\":\"blue\"},\"h\":{\"form\":\"table\",\"size\":\"small\",\"color\":\"red\"},\"i\":{\"form\":\"pyramid\",\"size\":\"large\",\"color\":\"yellow\"},\"j\":{\"form\":\"pyramid\",\"size\":\"small\",\"color\":\"red\"},\"k\":{\"form\":\"box\",\"size\":\"large\",\"color\":\"yellow\"},\"l\":{\"form\":\"box\",\"size\":\"large\",\"color\":\"red\"},\"m\":{\"form\":\"box\",\"size\":\"small\",\"color\":\"blue\"}},\"holding\":\"null\",\"utterance\":"+takeCmd +"}");	
+			} else {
+				//Add medium world later
+				jsinput   = (JSONObject) JSONValue.parse("{\"world\":[[\"e\"],[\"g\",\"l\"],[],[\"k\",\"m\",\"f\"],[]],\"objects\":{\"a\":{\"form\":\"brick\",\"size\":\"large\",\"color\":\"green\"},\"b\":{\"form\":\"brick\",\"size\":\"small\",\"color\":\"white\"},\"c\":{\"form\":\"plank\",\"size\":\"large\",\"color\":\"red\"},\"d\":{\"form\":\"plank\",\"size\":\"small\",\"color\":\"green\"},\"e\":{\"form\":\"ball\",\"size\":\"large\",\"color\":\"white\"},\"f\":{\"form\":\"ball\",\"size\":\"small\",\"color\":\"black\"},\"g\":{\"form\":\"table\",\"size\":\"large\",\"color\":\"blue\"},\"h\":{\"form\":\"table\",\"size\":\"small\",\"color\":\"red\"},\"i\":{\"form\":\"pyramid\",\"size\":\"large\",\"color\":\"yellow\"},\"j\":{\"form\":\"pyramid\",\"size\":\"small\",\"color\":\"red\"},\"k\":{\"form\":\"box\",\"size\":\"large\",\"color\":\"yellow\"},\"l\":{\"form\":\"box\",\"size\":\"large\",\"color\":\"red\"},\"m\":{\"form\":\"box\",\"size\":\"small\",\"color\":\"blue\"}},\"holding\":\"null\",\"utterance\":"+takeCmd +"}");
+			}
+			
+		} else {
+			 jsinput   = (JSONObject) JSONValue.parse(readFromStdin());
+		}
+        //
+		//JSONObject jsinput   = (JSONObject) JSONValue.parse(readFromStdin());
         JSONArray  utterance = (JSONArray)  jsinput.get("utterance");
         JSONArray  world     = (JSONArray)  jsinput.get("world");
         String     holding   = (String)     jsinput.get("holding");
         JSONObject objects   = (JSONObject) jsinput.get("objects");
-
+        if(consoleTest) {
+	        System.out.print("The world: "+world.toString()+"\n");
+	        System.out.print("The utterance: "+utterance.toString()+"\n");
+	        System.out.print("Holding: "+holding.toString()+"\n");
+	        System.out.print("Objects: "+objects.toString()+"\n");
+        }
         JSONObject result = new JSONObject();
         result.put("utterance", utterance);
-
+        //System.out.print(result+"\n");
+        
         // // This is how to get information about the top object in column 1:
         // JSONArray column = (JSONArray) world.get(1);
         // String topobject = (String) column.get(column.size() - 1);
@@ -98,6 +131,4 @@ public class Shrdlite {
         }
         return data.toString();
     }
-
 }
-
