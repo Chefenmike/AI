0a47e58d1db8976008953976450d4b90a0e6eab3
Sundin <sundin666@gmail.com>
2014-04-04 10:21:07 +0200

    bugfix in World (colour should be color in JSON)

--- src/group3/definitions/World.java
+++ src/group3/definitions/World.java
@@ -1,5 +1,8 @@
 package group3.definitions;
 
+import gnu.prolog.term.AtomicTerm;
+import gnu.prolog.term.Term;
+
 import java.util.ArrayList;
 
 import org.json.simple.JSONArray;
@@ -7,12 +10,12 @@ import org.json.simple.JSONObject;
 
 public class World {
 
-	private ArrayList worldRepresentationList;
+	private ArrayList<ArrayList<ObjectInWorld>> worldRepresentationList;
 
 	public World(JSONArray world, JSONObject objects) {
-		worldRepresentationList = new ArrayList<ObjectInWorld>();
+		worldRepresentationList = new ArrayList<ArrayList<ObjectInWorld>>();
 		for (int i = 0; i < world.size(); i++) {
-			ArrayList columnList = new ArrayList<ObjectInWorld>();
+			ArrayList<ObjectInWorld> columnList = new ArrayList<ObjectInWorld>();
 			JSONArray column = (JSONArray) world.get(i);
 
 			for (int j = 0; j < column.size(); j++) {
@@ -20,7 +23,7 @@ public class World {
 				ObjectInWorld oiw = new ObjectInWorld(
 						Shape.getShapeValueFromString((String) obj.get("form")),
 						Colour.getColourValueFromString((String) obj
-								.get("colour")), Size
+								.get("color")), Size
 								.getSizeValueFromString((String) obj
 										.get("size")));
 				columnList.add(oiw);
@@ -29,4 +32,26 @@ public class World {
 		}
 	}
 
+	public ObjectInWorld getObject(Shape shape, Colour colour, Size size, Quantifier quantifier, Location location) {
+		//in: term
+		return new ObjectInWorld(shape, colour, size);
+	}
+
+	public ArrayList<ObjectInWorld> getObjects(Term term) {
+		ArrayList<ObjectInWorld> returnList = new ArrayList<ObjectInWorld>();
+		
+		for (ArrayList<ObjectInWorld> a : worldRepresentationList) {
+			for(ObjectInWorld obj : a) {
+				
+			}
+		}
+		
+		// TODO Auto-generated method stub
+		return returnList;
+	}
+	
+	private String getAtomString(Term term) {
+		AtomicTerm t = (AtomicTerm) term;
+		return t.toString();
+	}
 }
