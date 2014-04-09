00254bd13be73b5d064345bd532b97804826f8e0
Sundin <sundin666@gmail.com>
2014-04-09 11:11:17 +0200

    Merged deBuggerNs changes into the master branch

--- src/group3/definitions/World.java
+++ src/group3/definitions/World.java
@@ -107,38 +107,156 @@ public class World {
 	 * @return true if the relation exist between object1 and any object in the list
 	 */
 	public boolean checkRelation(ObjectInWorld object1, RelativePosition rp, ArrayList<ObjectInWorld> otherObjects) {
-		for (ObjectInWorld object2 : otherObjects) {
-			if (checkRelation(object1, rp, object2)) {
+		if (otherObjects.isEmpty()) {
+			return checkRelation(object1, rp);
+		} else {
+			for (ObjectInWorld object2 : otherObjects) {
+				if (checkRelation(object1, rp, object2)) {
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+
+	/**
+	 * Checks if object1 is [RELATION] object2
+	 * @param object1
+	 * @param rp the relation
+	 * @param object2
+	 * @return
+	 */
+	public boolean checkRelation(ObjectInWorld object1, RelativePosition rp, ObjectInWorld object2) {
+		if (rp.equals(RelativePosition.INSIDE)) {
+			return isInside(object1, object2); 
+		} else if (rp.equals(RelativePosition.ONTOP)) {
+			return isOnTopOf(object1, object2);
+		} else if (rp.equals(RelativePosition.ABOVE)) {
+			return isAbove(object1, object2);
+		} else if (rp.equals(RelativePosition.UNDER)) {
+			return isUnder(object1, object2);
+		} else if (rp.equals(RelativePosition.BESIDE)) {
+			return isBeside(object1, object2);
+		} 
+
+		return false;
+	}
+	
+	/**
+	 * Checks unary relations (on floor)
+	 * @param object1
+	 * @param rp
+	 * @return
+	 */
+	public boolean checkRelation(ObjectInWorld object1, RelativePosition rp) {
+		if (rp.equals(RelativePosition.ONFLOOR)) {
+			return isOnFloor(object1);
+		}
+
+		return false;
+	}
+
+	private boolean isOnFloor(ObjectInWorld object1) {
+		for(ArrayList<ObjectInWorld> objList : worldRepresentationList){
+			if(!objList.isEmpty() && objList.get(0).equals(object1)) {
 				return true;
 			}
 		}
 		return false;
 	}
 
-	public boolean checkRelation(ObjectInWorld object1, RelativePosition rp, ObjectInWorld object2) {
-		if (rp.equals(RelativePosition.INSIDE)) {
-			return isInside(object1, object2); 
-		} else if (rp.equals(RelativePosition.ONTOP)) {
-			//TODO
-		} else if (rp.equals(RelativePosition.ABOVE)) {
-			//TODO
-		} else if (rp.equals(RelativePosition.UNDER)) {
-			//TODO
-		} else if (rp.equals(RelativePosition.BESIDE)) {
-			//TODO
-		} 
-
+	private boolean isInside(ObjectInWorld a, ObjectInWorld b) {
+		if (b.getShape()==Shape.BOX) {
+			return isOnTopOf(a, b);
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean isOnTopOf(ObjectInWorld b, ObjectInWorld a){
+		for(ArrayList<ObjectInWorld> objList : worldRepresentationList){
+			for(int i = 0; i < objList.size(); i++){
+				ObjectInWorld obj = objList.get(i);
+				if(obj.equals(a) && i != objList.size()-1){
+					return objList.get(i+1).equals(b);
+				}
+			}
+		}
 		return false;
 	}
 
-	//TODO: Implement this methods + methods for the other relations.
-	/**
-	 * checks if object1 is inside object2
-	 * @param object1
-	 * @param object2
-	 * @return true if object1 is inside object2
-	 */
-	private boolean isInside(ObjectInWorld object1, ObjectInWorld object2) {
-		return true;
+	public boolean isAbove(ObjectInWorld b, ObjectInWorld a){
+		for(ArrayList<ObjectInWorld> objList : worldRepresentationList){
+			for(int i = 0; i < objList.size(); i++){
+				ObjectInWorld obj = objList.get(i);
+				if(obj.equals(a)){
+					for(int j = i+1; j < objList.size(); j++){
+						if(objList.get(j).equals(b)){
+							return true;
+						}
+					}
+					return false;
+				}
+			}
+		}
+		return false;
 	}
+
+	public boolean isUnder(ObjectInWorld b, ObjectInWorld a){
+		for(ArrayList<ObjectInWorld> objList : worldRepresentationList){
+			for(int i = 0; i < objList.size(); i++){
+				ObjectInWorld obj = objList.get(i);
+				if(obj.equals(a)){
+					for(int j = i-1; j > -1; j--){
+						if(objList.get(j).equals(b)){
+							return true;
+						}
+					}
+					return false;
+				}
+			}
+		}
+		return false;
+	}
+
+	public boolean isBeside(ObjectInWorld b, ObjectInWorld a){
+		for(int i = 0; i < worldRepresentationList.size(); i++){
+			ArrayList<ObjectInWorld> objList = worldRepresentationList.get(i);
+			for(ObjectInWorld obj : objList){
+				if(obj.equals(a)){
+					return (worldRepresentationList.get(i-1).contains(b) || worldRepresentationList.get(i+1).contains(b));
+				}
+			}
+		}
+		return false;
+	}
+
+	public boolean isLeftOf(ObjectInWorld b, ObjectInWorld a){
+		for(int i = 0; i < worldRepresentationList.size(); i++){
+			ArrayList<ObjectInWorld> objList = worldRepresentationList.get(i);
+			for(ObjectInWorld obj : objList){
+				if(obj.equals(a)){
+					for(int j = i-1; j > -1; j--){
+						if(worldRepresentationList.get(j).contains(b)) return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	public boolean isRightOf(ObjectInWorld b, ObjectInWorld a){
+		for(int i = 0; i < worldRepresentationList.size(); i++){
+			ArrayList<ObjectInWorld> objList = worldRepresentationList.get(i);
+			for(ObjectInWorld obj : objList){
+				if(obj.equals(a)){
+					for(int j = i+1; j < worldRepresentationList.size(); j++){
+						if(worldRepresentationList.get(j).contains(b)) return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
 }
